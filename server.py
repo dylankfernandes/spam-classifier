24f88096afe42c0d1f2d9d314af50739fbefa64a
Dylan Fernandes <dylan.fernandes00@gmail.com>
2020-03-10 11:16:08 -0400

    setup starter application

--- server.py
+++ server.py
@@ -1,20 +1,10 @@
 from flask import Flask, request, render_template, jsonify
-import joblib
-import numpy as np
 
 app = Flask(__name__)
 
-model = joblib.load('models/saved/model.joblib')
-
 @app.route('/')
 def main():
-  return render_template('index.html')
-
-@app.route('/predict', methods=["POST"])
-def classify():
-  if request.method == "POST":
-    message = request.form['submission']
-    return render_template('index.html', classification=list(model.predict([message]))[0])
+  return "HooHacks + MLC@UVA Demo"
 
 if __name__ == '__main__':
   app.run(debug=True)
